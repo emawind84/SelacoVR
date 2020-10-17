d578ed998f2e78e719bfbe7f7a71752e27050958
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-10-17 10:18:37 +0200

    - use a few static asserts instead of runtime asserts in the VM's type code.

--- src/scripting/types.cpp
+++ src/scripting/types.cpp
@@ -1042,7 +1042,7 @@ PName::PName()
 {
 	mDescriptiveName = "Name";
 	Flags |= TYPE_IntNotInt;
-	assert(sizeof(FName) == alignof(FName));
+	static_assert(sizeof(FName) == alignof(FName), "Name not properly aligned");
 }
 
 //==========================================================================
@@ -1132,7 +1132,7 @@ PTextureID::PTextureID()
 {
 	mDescriptiveName = "TextureID";
 	Flags |= TYPE_IntNotInt;
-	assert(sizeof(FTextureID) == alignof(FTextureID));
+	static_assert(sizeof(FTextureID) == alignof(FTextureID), "TextureID not properly aligned");
 }
 
 //==========================================================================
@@ -1174,7 +1174,7 @@ PSound::PSound()
 {
 	mDescriptiveName = "Sound";
 	Flags |= TYPE_IntNotInt;
-	assert(sizeof(FSoundID) == alignof(FSoundID));
+	static_assert(sizeof(FSoundID) == alignof(FSoundID), "SoundID not properly aligned");
 }
 
 //==========================================================================
@@ -1223,7 +1223,7 @@ PColor::PColor()
 {
 	mDescriptiveName = "Color";
 	Flags |= TYPE_IntNotInt;
-	assert(sizeof(PalEntry) == alignof(PalEntry));
+	static_assert(sizeof(PalEntry) == alignof(PalEntry), "PalEntry not properly aligned");
 }
 
 /* PStateLabel *****************************************************************/
