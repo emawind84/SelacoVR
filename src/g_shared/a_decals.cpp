295ddd2a064689a2f8159f221d0f1c1161c5d1a5
alexey.lysiuk <alexey.lysiuk@gmail.com>
2020-11-01 12:17:40 +0200

    - do not keep impact decal that fails to stick to a wall
    Such invisible decals were not taken into account when calculating their total number
    Changing cl_maxdecals CVAR may not work as expected because of limit counter's inconsistent value
    
    https://forum.zdoom.org/viewtopic.php?t=70457

--- src/g_shared/a_decals.cpp
+++ src/g_shared/a_decals.cpp
@@ -644,6 +644,7 @@ DImpactDecal *DImpactDecal::StaticCreate (const FDecalTemplate *tpl, const DVect
 
 		if (!decal->StickToWall (wall, pos.X, pos.Y, ffloor).isValid())
 		{
+			decal->Destroy();
 			return NULL;
 		}
 
