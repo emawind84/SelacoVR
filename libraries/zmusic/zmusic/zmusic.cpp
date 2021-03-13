1c801f035c2c415ea2194fa8af21b50e30c64439
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-03-13 23:53:07 +0100

    - added ZMusic_GetDeviceType function to return the currently active MIDI synth.
    # Conflicts:
    #	libraries/zmusic/zmusic/zmusic.cpp
    #	libraries/zmusic/zmusic/zmusic.h

--- libraries/zmusic/zmusic/zmusic.cpp
+++ libraries/zmusic/zmusic/zmusic.cpp
@@ -435,6 +435,12 @@ DLL_EXPORT bool ZMusic_IsLooping(MusInfo *song)
 	return song->m_Looping;
 }
 
+DLL_EXPORT int ZMusic_GetDeviceType(MusInfo* song)
+{
+	if (!song) return false;
+	return song->GetDeviceType();
+}
+
 DLL_EXPORT bool ZMusic_IsMIDI(MusInfo *song)
 {
 	if (!song) return false;
