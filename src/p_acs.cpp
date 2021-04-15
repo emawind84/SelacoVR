14686b9a7f23df6b532cb6bc267b09a4f9370c04
Zandrewnum <andy.clarke99.ac@gmail.com>
2021-04-15 14:31:22 -0700

    Sound Class renovations
    - SoundClass is instantiated to "" by default. Since this property is only used when it is not empty (otherwise GetSoundClass just defaults to player), we can get away with this.
    - We may want the soundclass to remain the same if we explicitly set it to the same one that is currently used (say, we set SoundClass to "Caleb" so all other skins can use it)
    - GetActorProperty for APROP_SoundClass just calls GetSoundClass,
    - CheckActorProperty also just runs GetSoundClass
    - GetSoundClass is no longer a static method. We needed to access it in other places.
    - Made renovations to GetSoundClass. First of all, SoundClass is no longer instantiated there. Secondly, skinned sounds are now returned if SoundClass is empty. Thirdly, "sclass" in this method will return the default soundclass of the player pawn or SoundClass, depending on if SoundClass is empty. Finally, sclass will retrieve "player" if it is empty.
    
    # Conflicts:
    #	src/p_acs.cpp

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -4436,6 +4436,10 @@ void DLevelScript::DoSetActorProperty (AActor *actor, int property, int value)
 		{
 			if (actor->player != NULL)
 			{
+				if(!strcmp(FBehavior::StaticLookupString(value), ""))
+				{
+					actor->player->SoundClass = "";
+				}
 				actor->player->SoundClass = FBehavior::StaticLookupString(value);
 			}
 		}
@@ -4539,7 +4543,7 @@ int DLevelScript::GetActorProperty (int tid, int property)
 	case APROP_MaxStepHeight: return DoubleToACS(actor->MaxStepHeight);
 	case APROP_MaxDropOffHeight: return DoubleToACS(actor->MaxDropOffHeight);
 	case APROP_DamageType:	return GlobalACSStrings.AddString(actor->DamageType);
-	case APROP_SoundClass:	return GlobalACSStrings.AddString(actor->player->SoundClass.GetChars());
+	case APROP_SoundClass:	return GlobalACSStrings.AddString(GetSoundClass(actor));
 
 	default:				return 0;
 	}
@@ -4613,7 +4617,7 @@ int DLevelScript::CheckActorProperty (int tid, int property, int value)
 		case APROP_Species:		string = actor->GetSpecies(); break;
 		case APROP_NameTag:		string = actor->GetTag(); break;
 		case APROP_DamageType:	string = actor->DamageType; break;
-		case APROP_SoundClass:  string = actor->player->SoundClass; break;
+		case APROP_SoundClass:  string = GetSoundClass(actor); break;
 	}
 	if (string == NULL) string = "";
 	return (!stricmp(string, FBehavior::StaticLookupString(value)));
