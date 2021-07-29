bda1e7f77d20abbf4dba5fa651cd07cbe71872ff
drfrag <drfrag666@hotmail.com>
2021-07-29 23:53:51 +0200

    - Windows Server name correction.

--- src/win32/i_system.cpp
+++ src/win32/i_system.cpp
@@ -250,12 +250,12 @@ void I_DetectOS(void)
 			}
 			else if (info.dwMinorVersion == 4)
 			{
-				osname = (info.wProductType == VER_NT_WORKSTATION) ? "10 (beta)" : "Server 10 (beta)";
+				osname = (info.wProductType == VER_NT_WORKSTATION) ? "10 (beta)" : "Server 2016 (beta)";
 			}
 		}
 		else if (info.dwMajorVersion == 10)
 		{
-			osname = (info.wProductType == VER_NT_WORKSTATION) ? "10 (or higher)" : "Server 10 (or higher)";
+			osname = (info.wProductType == VER_NT_WORKSTATION) ? "10 (or higher)" : "Server 2016 (or higher)";
 			sys_ostype = 3; // modern OS
 		}
 		break;
