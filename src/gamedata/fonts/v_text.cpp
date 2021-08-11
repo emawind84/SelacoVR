3bc7d76703fdf33640eba6e1d6edea20448d4d9e
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-08-11 16:01:40 +0200

    - fixed line color handling in V_BreakLines.
    This cannot use the last color found while reading ahead - it must pick the last color of the string part that just got broken out into its own line when starting the next one.

--- src/gamedata/fonts/v_text.cpp
+++ src/gamedata/fonts/v_text.cpp
@@ -84,7 +84,7 @@ TArray<FBrokenLines> V_BreakLines (FFont *font, int maxwidth, const uint8_t *str
 			{
 				if (*string == '[')
 				{
-					const uint8_t *start = string;
+					const uint8_t* start = string;
 					while (*string != ']' && *string != '\0')
 					{
 						string++;
@@ -93,11 +93,6 @@ TArray<FBrokenLines> V_BreakLines (FFont *font, int maxwidth, const uint8_t *str
 					{
 						string++;
 					}
-					lastcolor = FString((const char *)start, string - start);
-				}
-				else
-				{
-					lastcolor = *string++;
 				}
 			}
 			continue;
@@ -124,6 +119,33 @@ TArray<FBrokenLines> V_BreakLines (FFont *font, int maxwidth, const uint8_t *str
 				space = string - 1;
 
 			auto index = Lines.Reserve(1);
+			for (const uint8_t* pos = start; pos < space; pos++)
+			{
+				if (*pos == TEXTCOLOR_ESCAPE)
+				{
+					pos++;
+					if (*pos)
+					{
+						if (*pos == '[')
+						{
+							const uint8_t* cstart = pos;
+							while (*pos != ']' && *pos != '\0')
+							{
+								pos++;
+							}
+							if (*pos != '\0')
+							{
+								pos++;
+							}
+							lastcolor = FString((const char*)cstart, pos - start);
+						}
+						else
+						{
+							lastcolor = *pos++;
+						}
+					}
+				}
+			}
 			breakit (&Lines[index], font, start, space, linecolor);
 			if (c == '\n' && !preservecolor)
 			{
