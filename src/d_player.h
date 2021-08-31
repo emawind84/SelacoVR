1726bbd1b9bdaa5d961ca6056f7a7bdc2d5f9ba2
Emanuele Disco <emawind84@gmail.com>
2021-08-31 22:33:06 +0900

    - disabled autoaim since not really needed in vr also it creates some problems when dealing with projectile spread

--- src/d_player.h
+++ src/d_player.h
@@ -180,20 +180,7 @@ struct userinfo_t : TMap<FName,FBaseCVar *>
 
 	double GetAimDist() const
 	{
-		if (dmflags2 & DF2_NOAUTOAIM)
-		{
-			return 0;
-		}
-
-		float aim = *static_cast<FFloatCVar *>(*CheckKey(NAME_Autoaim));
-		if (aim > 35 || aim < 0)
-		{
-			return 35.;
-		}
-		else
-		{
-			return aim;
-		}
+		return 0;
 	}
 	// Same but unfiltered.
 	double GetAutoaim() const
