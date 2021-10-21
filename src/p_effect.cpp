83e347164dec80cbc095bacbeac4cbff707e877e
Emanuele Disco <emawind84@gmail.com>
2021-10-22 01:47:26 +0900

    - fixed P_DrawRailTrail to work with offhand weapon

--- src/p_effect.cpp
+++ src/p_effect.cpp
@@ -659,12 +659,15 @@ void P_DrawRailTrail(AActor *source, TArray<SPortalHit> &portalhits, int color1,
 	{
 		if (!(flags & RAF_SILENT))
 		{
-			FSoundID sound;
+			FSoundID sound = 0;
 			
 			// Allow other sounds than 'weapons/railgf'!
 			if (!source->player) sound = source->AttackSound;
-			else if (source->player->ReadyWeapon) sound = source->player->ReadyWeapon->AttackSound;
-			else sound = 0;
+			else
+			{
+				AActor *weap = !!(flags & RAF_ISOFFHAND) ? source->player->OffhandWeapon : source->player->ReadyWeapon;
+				if (weap != nullptr) sound = weap->AttackSound;
+			}
 			if (!sound) sound = "weapons/railgf";
 
 			// The railgun's sound is special. It gets played from the
