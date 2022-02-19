77b0bf0962ea5063caefe9a2c78a4ac477384174
Emanuele Disco <emawind84@gmail.com>
2022-02-20 05:29:47 +0900

    - fixed crash when cmap is nullptr During my test the game was crashing on Tartarus map with fogmode enabled with access violation exception when trying to access the pointer cmap

--- src/gl/renderer/gl_lightdata.cpp
+++ src/gl/renderer/gl_lightdata.cpp
@@ -467,7 +467,7 @@ void gl_SetFog(int lightlevel, int rellight, bool fullbright, const FColormap *c
 	}
 	else
 	{
-		if ((glset.lightmode == 2 || (glset.lightmode >= 8 && cmap && cmap->BlendFactor > 0)) && fogcolor == 0)
+		if (cmap && (glset.lightmode == 2 || (glset.lightmode >= 8 && cmap->BlendFactor > 0)) && fogcolor == 0)
 		{
 			float light = gl_CalcLightLevel(lightlevel, rellight, false, cmap->BlendFactor);
 			gl_SetShaderLight(light, lightlevel);
