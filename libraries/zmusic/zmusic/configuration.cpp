4abbaeb314e2f93d90039af600e0d7206cf98c32
Emanuele Disco <emawind84@gmail.com>
2022-04-07 00:00:50 +0900

    - timidity_reverb_level min level set to 0, set to 9 by mistake? - fixed TimidityPlus_SetReverb executed before setting the value used inside the method

--- libraries/zmusic/zmusic/configuration.cpp
+++ libraries/zmusic/zmusic/configuration.cpp
@@ -479,15 +479,15 @@ DLL_EXPORT bool ChangeMusicSettingInt(EIntConfigKey key, MusInfo *currSong, int 
 
 		case zmusic_timidity_reverb:
 			if (value < 0 || value > 4) value = 0;
-			else TimidityPlus_SetReverb();
 			local_timidity_reverb = value;
+			TimidityPlus_SetReverb();
 			if (pRealValue) *pRealValue = value;
 			return false;
 
 		case zmusic_timidity_reverb_level:
 			if (value < 0 || value > 127) value = 0;
-			else TimidityPlus_SetReverb();
 			local_timidity_reverb_level = value;
+			TimidityPlus_SetReverb();
 			if (pRealValue) *pRealValue = value;
 			return false;
 
