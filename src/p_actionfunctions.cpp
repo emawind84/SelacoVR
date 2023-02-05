ZDoom/gzdoom

Changed static arrays to TArrays
- Made the models and skins arrays TArrays
- The issue I described with models not always reverting to default properly was caused by the fact I was unintentionally overwriting smf data. Now intermediate TArrays store the data before the loop instead of overwriting anything

src/playsim/p_actionfunctions.cpp
@@ -5055,8 +5055,13 @@ DEFINE_ACTION_FUNCTION(AActor, A_ChangeModel)
 
 	mobj->hasmodel = modeldef == nullptr && !mobj->hasmodel ? 1 : 0;
 	mobj->modelDef = modeldef;
-	mobj->models[modelindex] = model != nullptr ? FindModel(modelpath.GetChars(), model.GetChars()) : -1;
-	mobj->skins[skinindex] = skin != nullptr ? LoadSkin(skinpath.GetChars(), skin.GetChars()) : mobj->skins[skinindex] = FNullTextureID();
+
+	mobj->models.Delete(modelindex);
+	mobj->skins.Delete(skinindex);
+	if(model != nullptr) mobj->models.Insert(modelindex, FindModel(modelpath.GetChars(), model.GetChars()));
+	else mobj->models.Insert(modelindex, -1);
+	if(skin != nullptr) mobj->skins.Insert(skinindex, LoadSkin(skinpath.GetChars(), skin.GetChars()));
+	else mobj->skins.Insert(skinindex, FNullTextureID());
 
 	return 0;
 }

emawind84/SelacoVR

Refs: PR ZDoom/gzdoom#1651
Co-authored-by: Emanuele Disco <emawind84@gmail.com>

src/p_actionfunctions.cpp
@@ -5065,8 +5065,13 @@ DEFINE_ACTION_FUNCTION(AActor, A_ChangeModel)
 
 	mobj->hasmodel = modeldef == NAME_None && !mobj->hasmodel ? 1 : 0;
 	mobj->modelDef = modeldef;
-	mobj->models[modelindex] = model != NAME_None ? FindModel(modelpath.GetChars(), model.GetChars()) : -1;
-	mobj->skins[skinindex] = skin != NAME_None ? LoadSkin(skinpath.GetChars(), skin.GetChars()) : mobj->skins[skinindex] = FNullTextureID();
+
+	mobj->models.Delete(modelindex);
+	mobj->skins.Delete(skinindex);
+	if(model != NAME_None) mobj->models.Insert(modelindex, FindModel(modelpath.GetChars(), model.GetChars()));
+	else mobj->models.Insert(modelindex, -1);
+	if(skin != NAME_None) mobj->skins.Insert(skinindex, LoadSkin(skinpath.GetChars(), skin.GetChars()));
+	else mobj->skins.Insert(skinindex, FNullTextureID());
 
 	return 0;
 }