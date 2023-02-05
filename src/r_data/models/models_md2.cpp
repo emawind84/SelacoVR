johncurley/gzdoom

- got rid of the state variables in the model class entirely.
This is *not* the right way to reduce function parameter count. The data in question is far easier evaluated at the calling site and passed as a parameter.

src/common/models/models_md2.cpp
@@ -332,7 +332,7 @@ void FDMDModel::BuildVertexBuffer(FModelRenderer *renderer)
 //
 //===========================================================================
 
-void FDMDModel::AddSkins(uint8_t *hitlist)
+void FDMDModel::AddSkins(uint8_t *hitlist, const FTextureID*)
 {
 	for (int i = 0; i < info.numSkins; i++)
 	{
@@ -363,7 +363,7 @@ int FDMDModel::FindFrame(const char * name)
 //
 //===========================================================================
 
-void FDMDModel::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int frameno, int frameno2, double inter, int translation, const TArray<FTextureID>&)
+void FDMDModel::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int frameno, int frameno2, double inter, int translation, const FTextureID*)
 {
 	if (frameno >= info.numFrames || frameno2 >= info.numFrames) return;
 

src/common/models/models_md3.cpp
@@ -302,14 +302,13 @@ void FMD3Model::BuildVertexBuffer(FModelRenderer *renderer)
 //
 //===========================================================================
 
-void FMD3Model::AddSkins(uint8_t *hitlist)
+void FMD3Model::AddSkins(uint8_t *hitlist, const FTextureID* surfaceskinids)
 {
 	for (unsigned i = 0; i < Surfaces.Size(); i++)
 	{
-		int ssIndex = i + curMDLIndex * MD3_MAX_SURFACES;
-		if (curSpriteMDLFrame && curSpriteMDLFrame->surfaceskinIDs[ssIndex].isValid())
+		if (surfaceskinids && surfaceskinids[i].isValid())
 		{
-			hitlist[curSpriteMDLFrame->surfaceskinIDs[ssIndex].GetIndex()] |= FTextureManager::HIT_Flat;
+			hitlist[surfaceskinids[i].GetIndex()] |= FTextureManager::HIT_Flat;
 		}
 
 		MD3Surface * surf = &Surfaces[i];
@@ -344,7 +343,7 @@ int FMD3Model::FindFrame(const char * name)
 //
 //===========================================================================
 
-void FMD3Model::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int frameno, int frameno2, double inter, int translation, const TArray<FTextureID>& surfaceskinids)
+void FMD3Model::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int frameno, int frameno2, double inter, int translation, const FTextureID* surfaceskinids)
 {
 	if ((unsigned)frameno >= Frames.Size() || (unsigned)frameno2 >= Frames.Size()) return;
 
@@ -358,10 +357,9 @@ void FMD3Model::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int f
 		FGameTexture *surfaceSkin = skin;
 		if (!surfaceSkin)
 		{
-			int ssIndex = i + curMDLIndex * MD3_MAX_SURFACES;
-			if (surfaceskinids[ssIndex].isValid())
+			if (surfaceskinids && surfaceskinids[i].isValid())
 			{
-				surfaceSkin = TexMan.GetGameTexture(surfaceskinids[ssIndex], true);
+				surfaceSkin = TexMan.GetGameTexture(surfaceskinids[i], true);
 			}
 			else if (surf->numSkins > 0 && surf->Skins[0].isValid())
 			{

src/common/models/models_obj.cpp
@@ -628,7 +628,7 @@ int FOBJModel::FindFrame(const char* name)
  * @param inter The amount to interpolate the two frames.
  * @param translation The translation for the skin
  */
-void FOBJModel::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int frameno, int frameno2, double inter, int translation, const TArray<FTextureID>& surfaceskinids)
+void FOBJModel::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int frameno, int frameno2, double inter, int translation, const FTextureID* surfaceskinids)
 {
 	// Prevent the model from rendering if the frame number is < 0
 	if (frameno < 0 || frameno2 < 0) return;
@@ -640,10 +640,9 @@ void FOBJModel::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int f
 		FGameTexture *userSkin = skin;
 		if (!userSkin)
 		{
-			int ssIndex = i + curMDLIndex * MD3_MAX_SURFACES;
-			if (i < MD3_MAX_SURFACES && surfaceskinids[ssIndex].isValid())
+			if (surfaceskinids && surfaceskinids[i].isValid())
 			{
-				userSkin = TexMan.GetGameTexture(surfaceskinids[ssIndex], true);
+				userSkin = TexMan.GetGameTexture(surfaceskinids[i], true);
 			}
 			else if (surf->skin.isValid())
 			{
@@ -668,18 +667,17 @@ void FOBJModel::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int f
  *
  * @param hitlist The list of textures
  */
-void FOBJModel::AddSkins(uint8_t* hitlist)
+void FOBJModel::AddSkins(uint8_t* hitlist, const FTextureID* surfaceskinids)
 {
 	for (size_t i = 0; i < surfaces.Size(); i++)
 	{
-		size_t ssIndex = i + curMDLIndex * MD3_MAX_SURFACES;
-		if (curSpriteMDLFrame && i < MD3_MAX_SURFACES && curSpriteMDLFrame->surfaceskinIDs[ssIndex].isValid())
+		if (surfaceskinids && i < MD3_MAX_SURFACES && surfaceskinids[i].isValid())
 		{
 			// Precache skins manually reassigned by the user.
 			// On OBJs with lots of skins, such as Doom map OBJs exported from GZDB,
 			// there may be too many skins for the user to manually change, unless
 			// the limit is bumped or surfaceskinIDs is changed to a TArray<FTextureID>.
-			hitlist[curSpriteMDLFrame->surfaceskinIDs[ssIndex].GetIndex()] |= FTextureManager::HIT_Flat;
+			hitlist[surfaceskinids[i].GetIndex()] |= FTextureManager::HIT_Flat;
 			return; // No need to precache skin that was replaced
 		}
 

src/common/models/models_ue1.cpp
@@ -227,7 +227,7 @@ int FUE1Model::FindFrame( const char *name )
 	return -1;
 }
 
-void FUE1Model::RenderFrame( FModelRenderer *renderer, FGameTexture *skin, int frame, int frame2, double inter, int translation, const TArray<FTextureID>& surfaceskinids)
+void FUE1Model::RenderFrame( FModelRenderer *renderer, FGameTexture *skin, int frame, int frame2, double inter, int translation, const FTextureID* surfaceskinids)
 {
 	// the moment of magic
 	if ( (frame >= numFrames) || (frame2 >= numFrames) ) return;
@@ -246,9 +246,8 @@ void FUE1Model::RenderFrame( FModelRenderer *renderer, FGameTexture *skin, int f
 		FGameTexture *sskin = skin;
 		if ( !sskin )
 		{
-			int ssIndex = groups[i].texNum + curMDLIndex * MD3_MAX_SURFACES;
-			if (surfaceskinids[ssIndex].isValid())
-				sskin = TexMan.GetGameTexture(surfaceskinids[ssIndex], true);
+			if (surfaceskinids && surfaceskinids[i].isValid())
+				sskin = TexMan.GetGameTexture(surfaceskinids[i], true);
 			if ( !sskin )
 			{
 				vofs += vsize;
@@ -305,13 +304,12 @@ void FUE1Model::BuildVertexBuffer( FModelRenderer *renderer )
 	vbuf->UnlockVertexBuffer();
 }
 
-void FUE1Model::AddSkins( uint8_t *hitlist )
+void FUE1Model::AddSkins( uint8_t *hitlist, const FTextureID* surfaceskinids)
 {
 	for (int i = 0; i < numGroups; i++)
 	{
-		int ssIndex = groups[i].texNum + curMDLIndex * MD3_MAX_SURFACES;
-		if (curSpriteMDLFrame && curSpriteMDLFrame->surfaceskinIDs[ssIndex].isValid())
-			hitlist[curSpriteMDLFrame->surfaceskinIDs[ssIndex].GetIndex()] |= FTextureManager::HIT_Flat;
+		if (surfaceskinids && surfaceskinids[i].isValid())
+			hitlist[surfaceskinids[i].GetIndex()] |= FTextureManager::HIT_Flat;
 	}
 }
 

src/common/models/models_voxel.cpp
@@ -356,7 +356,7 @@ void FVoxelModel::BuildVertexBuffer(FModelRenderer *renderer)
 //
 //===========================================================================
 
-void FVoxelModel::AddSkins(uint8_t *hitlist)
+void FVoxelModel::AddSkins(uint8_t *hitlist, const FTextureID*)
 {
 	hitlist[mPalette.GetIndex()] |= FTextureManager::HIT_Flat;
 }
@@ -400,7 +400,7 @@ float FVoxelModel::getAspectFactor(float stretch)
 //
 //===========================================================================
 
-void FVoxelModel::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int frame, int frame2, double inter, int translation, const TArray<FTextureID>&)
+void FVoxelModel::RenderFrame(FModelRenderer *renderer, FGameTexture * skin, int frame, int frame2, double inter, int translation, const FTextureID*)
 {
 	renderer->SetMaterial(skin, true, translation);
 	renderer->SetupFrame(this, 0, 0, 0);

emawind84/SelacoVR

src/r_data/models/models_md2.cpp
@@ -329,7 +329,7 @@ void FDMDModel::BuildVertexBuffer(FModelRenderer *renderer)
 //
 //===========================================================================
 
-void FDMDModel::AddSkins(uint8_t *hitlist)
+void FDMDModel::AddSkins(uint8_t *hitlist, const FTextureID*)
 {
 	for (int i = 0; i < info.numSkins; i++)
 	{
@@ -360,7 +360,7 @@ int FDMDModel::FindFrame(const char * name)
 //
 //===========================================================================
 
-void FDMDModel::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frameno, int frameno2, double inter, int translation, const TArray<FTextureID>&)
+void FDMDModel::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frameno, int frameno2, double inter, int translation, const FTextureID*)
 {
 	if (frameno >= info.numFrames || frameno2 >= info.numFrames) return;
 

src/r_data/models/models_md3.cpp
@@ -302,14 +302,13 @@ void FMD3Model::BuildVertexBuffer(FModelRenderer *renderer)
 //
 //===========================================================================
 
-void FMD3Model::AddSkins(uint8_t *hitlist)
+void FMD3Model::AddSkins(uint8_t *hitlist, const FTextureID* surfaceskinids)
 {
 	for (unsigned i = 0; i < Surfaces.Size(); i++)
 	{
-		int ssIndex = i + curMDLIndex * MD3_MAX_SURFACES;
-		if (curSpriteMDLFrame && curSpriteMDLFrame->surfaceskinIDs[ssIndex].isValid())
+		if (surfaceskinids && surfaceskinids[i].isValid())
 		{
-			hitlist[curSpriteMDLFrame->surfaceskinIDs[ssIndex].GetIndex()] |= FTextureManager::HIT_Flat;
+			hitlist[surfaceskinids[i].GetIndex()] |= FTextureManager::HIT_Flat;
 		}
 
 		MD3Surface * surf = &Surfaces[i];
@@ -344,7 +343,7 @@ int FMD3Model::FindFrame(const char * name)
 //
 //===========================================================================
 
-void FMD3Model::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frameno, int frameno2, double inter, int translation, const TArray<FTextureID>& surfaceskinids)
+void FMD3Model::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frameno, int frameno2, double inter, int translation, const FTextureID* surfaceskinids)
 {
 	if ((unsigned)frameno >= Frames.Size() || (unsigned)frameno2 >= Frames.Size()) return;
 
@@ -358,10 +357,9 @@ void FMD3Model::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frame
 		FTexture *surfaceSkin = skin;
 		if (!surfaceSkin)
 		{
-			int ssIndex = i + curMDLIndex * MD3_MAX_SURFACES;
-			if (surfaceskinids[ssIndex].isValid())
+			if (surfaceskinids && surfaceskinids[i].isValid())
 			{
-				surfaceSkin = TexMan(surfaceskinids[ssIndex]);
+				surfaceSkin = TexMan(surfaceskinids[i]);
 			}
 			else if (surf->numSkins > 0 && surf->Skins[0].isValid())
 			{

src/r_data/models/models_obj.cpp
@@ -624,7 +624,7 @@ int FOBJModel::FindFrame(const char* name)
  * @param inter Unused
  * @param translation The translation for the skin
  */
-void FOBJModel::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frameno, int frameno2, double inter, int translation, const TArray<FTextureID>& surfaceskinids)
+void FOBJModel::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frameno, int frameno2, double inter, int translation, const FTextureID* surfaceskinids)
 {
 	for (unsigned int i = 0; i < surfaces.Size(); i++)
 	{
@@ -633,10 +633,9 @@ void FOBJModel::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frame
 		FTexture *userSkin = skin;
 		if (!userSkin)
 		{
-			int ssIndex = i + curMDLIndex * MD3_MAX_SURFACES;
-			if (i < MD3_MAX_SURFACES && surfaceskinids[ssIndex].isValid())
+			if (surfaceskinids && surfaceskinids[i].isValid())
 			{
-				userSkin = TexMan(surfaceskinids[ssIndex]);
+				userSkin = TexMan(surfaceskinids[i]);
 			}
 			else if (surf->skin.isValid())
 			{
@@ -661,18 +660,17 @@ void FOBJModel::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frame
  *
  * @param hitlist The list of textures
  */
-void FOBJModel::AddSkins(uint8_t* hitlist)
+void FOBJModel::AddSkins(uint8_t* hitlist, const FTextureID* surfaceskinids)
 {
 	for (size_t i = 0; i < surfaces.Size(); i++)
 	{
-		size_t ssIndex = i + curMDLIndex * MD3_MAX_SURFACES;
-		if (curSpriteMDLFrame && i < MD3_MAX_SURFACES && curSpriteMDLFrame->surfaceskinIDs[ssIndex].isValid())
+		if (surfaceskinids && i < MD3_MAX_SURFACES && surfaceskinids[i].isValid())
 		{
 			// Precache skins manually reassigned by the user.
 			// On OBJs with lots of skins, such as Doom map OBJs exported from GZDB,
 			// there may be too many skins for the user to manually change, unless
 			// the limit is bumped or surfaceskinIDs is changed to a TArray<FTextureID>.
-			hitlist[curSpriteMDLFrame->surfaceskinIDs[ssIndex].GetIndex()] |= FTextureManager::HIT_Flat;
+			hitlist[surfaceskinids[i].GetIndex()] |= FTextureManager::HIT_Flat;
 			return; // No need to precache skin that was replaced
 		}
 

src/r_data/models/models_ue1.cpp
@@ -221,7 +221,7 @@ int FUE1Model::FindFrame( const char *name )
 	return -1;
 }
 
-void FUE1Model::RenderFrame( FModelRenderer *renderer, FTexture *skin, int frame, int frame2, double inter, int translation, const TArray<FTextureID>& surfaceskinids)
+void FUE1Model::RenderFrame( FModelRenderer *renderer, FTexture *skin, int frame, int frame2, double inter, int translation, const FTextureID* surfaceskinids)
 {
 	// the moment of magic
 	if ( (frame >= numFrames) || (frame2 >= numFrames) ) return;
@@ -240,9 +240,8 @@ void FUE1Model::RenderFrame( FModelRenderer *renderer, FTexture *skin, int frame
 		FTexture *sskin = skin;
 		if ( !sskin )
 		{
-			int ssIndex = groups[i].texNum + curMDLIndex * MD3_MAX_SURFACES;
-			if (surfaceskinids[ssIndex].isValid())
-				sskin = TexMan(surfaceskinids[ssIndex]);
+			if (surfaceskinids && surfaceskinids[i].isValid())
+				sskin = TexMan(surfaceskinids[i]);
 			if ( !sskin )
 			{
 				vofs += vsize;
@@ -299,13 +298,12 @@ void FUE1Model::BuildVertexBuffer( FModelRenderer *renderer )
 	vbuf->UnlockVertexBuffer();
 }
 
-void FUE1Model::AddSkins( uint8_t *hitlist )
+void FUE1Model::AddSkins( uint8_t *hitlist, const FTextureID* surfaceskinids)
 {
 	for (int i = 0; i < numGroups; i++)
 	{
-		int ssIndex = groups[i].texNum + curMDLIndex * MD3_MAX_SURFACES;
-		if (curSpriteMDLFrame && curSpriteMDLFrame->surfaceskinIDs[ssIndex].isValid())
-			hitlist[curSpriteMDLFrame->surfaceskinIDs[ssIndex].GetIndex()] |= FTextureManager::HIT_Flat;
+		if (surfaceskinids && surfaceskinids[i].isValid())
+			hitlist[surfaceskinids[i].GetIndex()] |= FTextureManager::HIT_Flat;
 	}
 }
 

src/r_data/models/models_voxel.cpp
@@ -362,7 +362,7 @@ void FVoxelModel::BuildVertexBuffer(FModelRenderer *renderer)
 //
 //===========================================================================
 
-void FVoxelModel::AddSkins(uint8_t *hitlist)
+void FVoxelModel::AddSkins(uint8_t *hitlist, const FTextureID*)
 {
 	hitlist[mPalette.GetIndex()] |= FTextureManager::HIT_Flat;
 }
@@ -406,7 +406,7 @@ float FVoxelModel::getAspectFactor()
 //
 //===========================================================================
 
-void FVoxelModel::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frame, int frame2, double inter, int translation, const TArray<FTextureID>&)
+void FVoxelModel::RenderFrame(FModelRenderer *renderer, FTexture * skin, int frame, int frame2, double inter, int translation, const FTextureID*)
 {
 	renderer->SetMaterial(skin, true, translation);
 	GetVertexBuffer(renderer)->SetupFrame(renderer, 0, 0, 0);