johncurley/gzdoom

- added palette shader and fixed a few things.

src/gl/textures/gl_material.cpp
@@ -327,7 +327,11 @@ FMaterial::FMaterial(FTexture * tx, bool expanded)
 	}
 	else
 	*/
-	if (tx->bWarped)
+	if (tx->UseType == ETextureType::SWCanvas && tx->WidthBits == 0)
+	{
+		mShaderIndex = SHADER_Paletted;
+	}
+	else if (tx->bWarped)
 	{
 		mShaderIndex = tx->bWarped; // This picks SHADER_Warp1 or SHADER_Warp2
 		tx->shaderspeed = static_cast<FWarpTexture*>(tx)->GetSpeed();
@@ -620,6 +624,7 @@ void FMaterial::Bind(int clampmode, int translation)
 	int usebright = false;
 	int maxbound = 0;
 
+	if (tex->UseType == ETextureType::SWCanvas) clampmode = CLAMP_NOFILTER;
 	if (tex->bHasCanvas) clampmode = CLAMP_CAMTEX;
 	else if (tex->bWarped && clampmode <= CLAMP_XY) clampmode = CLAMP_NONE;
 

emawind84/SelacoVR

src/gl/textures/gl_material.cpp
@@ -259,7 +259,11 @@ FMaterial::FMaterial(FTexture * tx, bool expanded)
 	mShaderIndex = SHADER_Default;
 	tex = tx;
 
-	if (tx->bHasCanvas)
+	if (tx->UseType == ETextureType::SWCanvas && tx->WidthBits == 0)
+	{
+		mShaderIndex = SHADER_Paletted;
+	}
+	else if (tx->bHasCanvas)
 	{
 		if (tx->shaderindex >= FIRST_USER_SHADER)
 		{
@@ -570,6 +574,7 @@ void FMaterial::InitGlobalState()
 
 void FMaterial::Bind(int clampmode, int translation)
 {
+	if (tex->UseType == ETextureType::SWCanvas) clampmode = CLAMP_NOFILTER;
 	if (tex->bHasCanvas) clampmode = CLAMP_CAMTEX;
 	else if ((tex->bWarped || tex->shaderindex >= FIRST_USER_SHADER) && clampmode <= CLAMP_XY) clampmode = CLAMP_NONE;
 	