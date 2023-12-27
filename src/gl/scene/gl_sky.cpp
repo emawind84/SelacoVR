johncurley/gzdoom

- GLWall is mostly clean, except some smaller things in gl_sky.cpp

src/gl/scene/gl_sky.cpp
@@ -26,11 +26,15 @@
 #include "r_utility.h"
 #include "doomdata.h"
 #include "g_levellocals.h"
-
-#include "gl/renderer/gl_lightdata.h"
-#include "gl/scene/gl_drawinfo.h"
-#include "gl/scene/gl_portal.h"
+#include "p_lnspec.h"
+#include "hwrenderer/scene/hw_drawinfo.h"
+#include "hwrenderer/scene/hw_drawstructs.h"
+#include "hwrenderer/scene/hw_portal.h"
+#include "hwrenderer/utility/hw_lighting.h"
 #include "hwrenderer/textures/hw_material.h"
+
+#include "gl/renderer/gl_renderer.h"
+#include "gl/scene/gl_portal.h"
 
 CVAR(Bool,gl_noskyboxes, false, 0)
 

emawind84/SelacoVR

src/gl/scene/gl_sky.cpp
@@ -26,12 +26,15 @@
 #include "r_utility.h"
 #include "doomdata.h"
 #include "g_levellocals.h"
-
-#include "gl/renderer/gl_lightdata.h"
-#include "gl/renderer/gl_renderstate.h"
-#include "gl/scene/gl_drawinfo.h"
-#include "gl/scene/gl_portal.h"
+#include "p_lnspec.h"
+#include "hwrenderer/scene/hw_drawinfo.h"
+#include "hwrenderer/scene/hw_drawstructs.h"
+#include "hwrenderer/scene/hw_portal.h"
+#include "hwrenderer/utility/hw_lighting.h"
 #include "hwrenderer/textures/hw_material.h"
+
+#include "gl/renderer/gl_renderstate.h"
+#include "gl/scene/gl_portal.h"
 
 CVAR(Bool,gl_noskyboxes, false, 0)
 CUSTOM_CVAR(Bool, gl_skydome, true, CVAR_GLOBALCONFIG|CVAR_ARCHIVE)