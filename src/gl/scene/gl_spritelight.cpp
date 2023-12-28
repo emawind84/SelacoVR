johncurley/gzdoom

- reduced gl_spritelight.cpp to pure data setup so that it can be moved out of gl/. - added thread_local to some static arrays being used for setting up dynamic lights.
Right now it's of little consequence but these will have to be maintained per thread if the render data setup is done by worker tasks.

src/gl/scene/gl_spritelight.cpp
@@ -54,13 +54,13 @@ T smoothstep(const T edge0, const T edge1, const T x)
 //
 //==========================================================================
 
-void gl_SetDynSpriteLight(AActor *self, float x, float y, float z, subsector_t * subsec)
+void hw_GetDynSpriteLight(AActor *self, float x, float y, float z, subsector_t * subsec, float *out)
 {
 	ADynamicLight *light;
 	float frac, lr, lg, lb;
 	float radius;
-	float out[3] = { 0.0f, 0.0f, 0.0f };
 	
+	out[0] = out[1] = out[2] = 0.f;
 	// Go through both light lists
 	FLightNode * node = subsec->lighthead;
 	while (node)
@@ -131,18 +131,17 @@ void gl_SetDynSpriteLight(AActor *self, float x, float y, float z, subsector_t *
 		}
 		node = node->nextLight;
 	}
-	gl_RenderState.SetDynLight(out[0], out[1], out[2]);
 }
 
-void gl_SetDynSpriteLight(AActor *thing, particle_t *particle)
+void hw_GetDynSpriteLight(AActor *thing, particle_t *particle, float *out)
 {
 	if (thing != NULL)
 	{
-		gl_SetDynSpriteLight(thing, thing->X(), thing->Y(), thing->Center(), thing->subsector);
+		hw_GetDynSpriteLight(thing, thing->X(), thing->Y(), thing->Center(), thing->subsector, out);
 	}
 	else if (particle != NULL)
 	{
-		gl_SetDynSpriteLight(NULL, particle->Pos.X, particle->Pos.Y, particle->Pos.Z, particle->subsector);
+		hw_GetDynSpriteLight(NULL, particle->Pos.X, particle->Pos.Y, particle->Pos.Z, particle->subsector, out);
 	}
 }
 
@@ -189,31 +188,19 @@ void BSPWalkCircle(float x, float y, float radiusSquared, const Callback &callba
 		BSPNodeWalkCircle(level.HeadNode(), x, y, radiusSquared, callback);
 }
 
-int gl_SetDynModelLight(AActor *self, int dynlightindex)
+// static so that we build up a reserve (memory allocations stop)
+// For multithread processing each worker thread needs its own copy, though.
+static thread_local TArray<ADynamicLight*> addedLightsArray; 
+
+void hw_GetDynModelLight(AActor *self, FDynLightData &modellightdata)
 {
-	static FDynLightData modellightdata;	// If this ever gets multithreaded, this variable must either be made non-static or thread_local.
-
-	// For deferred light mode this function gets called twice. First time for list upload, and second for draw.
-	if (gl.lightmethod == LM_DEFERRED && dynlightindex != -1)
-	{
-		gl_RenderState.SetDynLight(0, 0, 0);
-		return dynlightindex;
-	}
-
-	// Legacy render path gets the old flat model light
-	if (gl.lightmethod == LM_LEGACY)
-	{
-		gl_SetDynSpriteLight(self, nullptr);
-		return -1;
-	}
-
 	modellightdata.Clear();
 
 	if (self)
 	{
-		static std::vector<ADynamicLight*> addedLights; // static so that we build up a reserve (memory allocations stop)
+		auto &addedLights = addedLightsArray;	// avoid going through the thread local storage for each use.
 
-		addedLights.clear();
+		addedLights.Clear();
 
 		float x = self->X();
 		float y = self->Y();
@@ -240,7 +227,7 @@ int gl_SetDynModelLight(AActor *self, int dynlightindex)
 						if (std::find(addedLights.begin(), addedLights.end(), light) == addedLights.end()) // Check if we already added this light from a different subsector
 						{
 							modellightdata.AddLightToList(group, light);
-							addedLights.push_back(light);
+							addedLights.Push(light);
 						}
 					}
 				}
@@ -248,12 +235,4 @@ int gl_SetDynModelLight(AActor *self, int dynlightindex)
 			}
 		});
 	}
-
-	dynlightindex = GLRenderer->mLights->UploadLights(modellightdata);
-
-	if (gl.lightmethod != LM_DEFERRED)
-	{
-		gl_RenderState.SetDynLight(0, 0, 0);
-	}
-	return dynlightindex;
 }

emawind84/SelacoVR

src/gl/scene/gl_spritelight.cpp
@@ -55,13 +55,13 @@ T smoothstep(const T edge0, const T edge1, const T x)
 //
 //==========================================================================
 
-void gl_SetDynSpriteLight(AActor *self, float x, float y, float z, subsector_t * subsec)
+void hw_GetDynSpriteLight(AActor *self, float x, float y, float z, subsector_t * subsec, float *out)
 {
 	FDynamicLight *light;
 	float frac, lr, lg, lb;
 	float radius;
-	float out[3] = { 0.0f, 0.0f, 0.0f };
 	
+	out[0] = out[1] = out[2] = 0.f;
 	// Go through both light lists
 	FLightNode * node = subsec->lighthead;
 	while (node)
@@ -133,46 +133,33 @@ void gl_SetDynSpriteLight(AActor *self, float x, float y, float z, subsector_t *
 		}
 		node = node->nextLight;
 	}
-	gl_RenderState.SetDynLight(out[0], out[1], out[2]);
 }
 
-void gl_SetDynSpriteLight(AActor *thing, particle_t *particle)
+void hw_GetDynSpriteLight(AActor *thing, particle_t *particle, float *out)
 {
 	if (thing != NULL)
 	{
-		gl_SetDynSpriteLight(thing, (float)thing->X(), (float)thing->Y(), (float)thing->Center(), thing->subsector);
+		hw_GetDynSpriteLight(thing, (float)thing->X(), (float)thing->Y(), (float)thing->Center(), thing->subsector, out);
 	}
 	else if (particle != NULL)
 	{
-		gl_SetDynSpriteLight(NULL, (float)particle->Pos.X, (float)particle->Pos.Y, (float)particle->Pos.Z, particle->subsector);
+		hw_GetDynSpriteLight(NULL, (float)particle->Pos.X, (float)particle->Pos.Y, (float)particle->Pos.Z, particle->subsector, out);
 	}
 }
 
-int gl_SetDynModelLight(AActor *self, int dynlightindex)
+// static so that we build up a reserve (memory allocations stop)
+// For multithread processing each worker thread needs its own copy, though.
+static thread_local TArray<FDynamicLight*> addedLightsArray; 
+
+void hw_GetDynModelLight(AActor *self, FDynLightData &modellightdata)
 {
-	static FDynLightData modellightdata;	// If this ever gets multithreaded, this variable must either be made non-static or thread_local.
-
-	// For deferred light mode this function gets called twice. First time for list upload, and second for draw.
-	if (gl.lightmethod == LM_DEFERRED && dynlightindex != -1)
-	{
-		gl_RenderState.SetDynLight(0, 0, 0);
-		return dynlightindex;
-	}
-
-	// Legacy render path gets the old flat model light
-	if (gl.lightmethod == LM_LEGACY)
-	{
-		gl_SetDynSpriteLight(self, nullptr);
-		return -1;
-	}
-
 	modellightdata.Clear();
 
 	if (self)
 	{
-		static std::vector<FDynamicLight*> addedLights; // static so that we build up a reserve (memory allocations stop)
+		auto &addedLights = addedLightsArray;	// avoid going through the thread local storage for each use.
 
-		addedLights.clear();
+		addedLights.Clear();
 
 		float x = (float)self->X();
 		float y = (float)self->Y();
@@ -205,7 +192,7 @@ int gl_SetDynModelLight(AActor *self, int dynlightindex)
 						if (std::find(addedLights.begin(), addedLights.end(), light) == addedLights.end()) // Check if we already added this light from a different subsector
 						{
 							modellightdata.AddLightToList(group, light);
-							addedLights.push_back(light);
+							addedLights.Push(light);
 						}
 					}
 				}
@@ -213,12 +200,4 @@ int gl_SetDynModelLight(AActor *self, int dynlightindex)
 			}
 		});
 	}
-
-	dynlightindex = GLRenderer->mLights->UploadLights(modellightdata);
-
-	if (gl.lightmethod != LM_DEFERRED)
-	{
-		gl_RenderState.SetDynLight(0, 0, 0);
-	}
-	return dynlightindex;
 }