88481144d0423139b953d8e590a12e8c7d13e80a
Emanuele Disco <emawind84@gmail.com>
2024-02-01 14:14:03 +0900

    Revert "- added printlocalized CCMD to check strings in-game."
    This reverts commit 74d8fa8a28175f10130f656df799ce8a264dc714.

--- src/gamedata/stringtable.cpp
+++ src/gamedata/stringtable.cpp
@@ -44,8 +44,6 @@
 #include "gi.h"
 #include "d_player.h"
 #include "xlsxread/xlsxio_read.h"
-#include "c_cvars.h"
-#include "gstrings.h"
 
 EXTERN_CVAR(String, language)
 
@@ -565,22 +563,4 @@ const char *StringMap::MatchString (const char *string) const
 		}
 	}
 	return nullptr;
-}
-
-CCMD(printlocalized)
-{
-	if (argv.argc() > 1)
-	{
-		if (argv.argc() > 2)
-		{
-			FString lang = argv[2];
-			lang.ToLower();
-			if (lang.Len() >= 2)
-			{
-				Printf("%s\n", GStrings.GetLanguageString(argv[1], MAKE_ID(lang[0], lang[1], lang[2], 0)));
-				return;
-			}
-		}
-		Printf("%s\n", GStrings(argv[1]));
-	}
 }
