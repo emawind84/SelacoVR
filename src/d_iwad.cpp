bcc0c491abced2e77731611bffddc7dae36441cd
Emanuele Disco <emawind84@gmail.com>
2024-02-07 18:17:07 +0900

    Revert "Added detection for IWADs downloaded via the Bethesda.net Launcher"
    This reverts commit f0e2f8eb50ea84a6233987881ad68aa6302f3a92.

--- src/d_iwad.cpp
+++ src/d_iwad.cpp
@@ -438,7 +438,6 @@ void FIWadManager::CollectSearchPaths()
 	}
 	mSearchPaths.Append(I_GetGogPaths());
 	mSearchPaths.Append(I_GetSteamPath());
-	mSearchPaths.Append(I_GetBethesdaPath());
 
 	// Unify and remove trailing slashes
 	for (auto &str : mSearchPaths)
