5694feef61111e66ae97ef8dc4c43be668174ffa
Emanuele Disco <emawind84@gmail.com>
2024-03-19 13:30:09 +0900

    feat: enable hardware clip planes and cull distances for mobile if the extension is present Also add the NO_CLIPDISTANCE_SUPPORT definition if RFL_NO_CLIP_PLANES is present

--- src/common/rendering/gl_load/gl_interface.cpp
+++ src/common/rendering/gl_load/gl_interface.cpp
@@ -140,9 +140,9 @@ void gl_LoadExtensions()
 
 #ifdef __MOBILE__
 	gl_version = 3.31;
-	gl.flags |= RFL_NO_CLIP_PLANES;
 	gl.flags |= RFL_INVALIDATE_BUFFER;
 	gl.flags |= RFL_SHADER_STORAGE_BUFFER;
+	if (!CheckExtension("GL_EXT_clip_cull_distance")) gl.flags |= RFL_NO_CLIP_PLANES;
 #endif
 	// Don't even start if it's lower than 2.0 or no framebuffers are available (The framebuffer extension is needed for glGenerateMipmapsEXT!)
 	if (gl_version < 3.3f)
