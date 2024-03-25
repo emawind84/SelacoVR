abaf27675a49eedca84af168785192fed59cbe51
Emanuele Disco <emawind84@gmail.com>
2024-03-25 10:32:20 +0900

    build: always output the long format for the tag when updating gitinfo.h

--- tools/updaterevision/UpdateRevision.cmake
+++ tools/updaterevision/UpdateRevision.cmake
@@ -16,7 +16,7 @@ endmacro()
 # from source repository.  If anything goes wrong return something in "Error."
 function(query_repo_info)
 	execute_process(
-		COMMAND git describe --tags --dirty=-m
+		COMMAND git describe --tags --dirty=-m --long
 		RESULT_VARIABLE Error
 		OUTPUT_VARIABLE Tag
 		ERROR_QUIET
